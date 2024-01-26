736c0fb88dfc4f78ba209b55aa2876a4744e95f7
VIGNESH-BOOPATHI <vigneshboopathi2002@gmail.com>
2024-01-26 13:35:53 +0530

    this is assingment-day03

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 var request =new XMLHttpRequest();
+
 request.open("GET","https://restcountries.com/v3.1/all")
+
+request.send();
+
+request.onload =function(){
+    var res = JSON.parse(request.response);
+    console.log(res);
+    
+    
+    for(var i =0;i<res.length;i++){
+        
+        console.log(res[i].flags.png+"\n"+
+        "country: "+res[i].name.common+"\n"+"Region: "+ 
+        res[i].region+"\n"+"Sub-Region: "+res[i].subregion+"\n"+"Population: "+
+        res[i].population+"\n"
+
+        )
+        
+
+    }
+    
+}
